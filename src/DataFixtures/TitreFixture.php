ffb4ed4017e6a9fd035699e0c568e3e800f949da
Nerpp <regiaurelien@gmail.com>
2019-03-05 17:29:22 +0100

    Ajout de faker

--- src/DataFixtures/TitreFixture.php
+++ src/DataFixtures/TitreFixture.php
@@ -4,13 +4,20 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Titre;
 
 class TitreFixture extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for($i = 1;$i <= 10;$i++){
+            $article = new Article();
+            $article->settitre("Titre de l'article n°$i");
+            $article->setarticle("<p>Contenu de l'article $i</p>");
+            $article->setimage("http://placehold.it/350x150");
+            $article->setdate(new \DateTime());
+            $manager->persist($article);
+        }
 
         $manager->flush();
     }
